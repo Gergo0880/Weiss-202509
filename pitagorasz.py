65d30eb6332109351a2ffb03da083f99d49fcf41
Gergo0880 <peto.gergely@wm-iskola.hu>
2025-09-19 09:41:02 +0200

    Create pitagorasz.py

---
+++ pitagorasz.py
@@ -0,0 +1,6 @@
+from math import*
+print('1.feladat: Pitagorasz tétel')
+a=float(input('Kérem a háromszög egyik befogóját: '))
+b=float(input('Kérem a háromszög egyik befogóját: '))
+c=sqrt(pow(a,2)+pow(b,2))
+print(f'Az átfogó nagysága: {c}')
